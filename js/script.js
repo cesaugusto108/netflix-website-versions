f8624fa3d1444b39ac3e2236f72355e3212908b7
César Augusto <ces.augusto108@gmail.com>
2022-02-22 19:47:11 -0300

    Ajustes para mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const nav = document.querySelector("nav");
+// efeitos na navbar
+
+const nav = document.querySelector(".navbar");
 
 function navChange() {
       if (scrollY > 1344) {
@@ -11,3 +13,20 @@ function navChange() {
 }
 
 window.addEventListener("scroll", navChange);
+
+// tamanho do input da classe form-control
+
+const formInput1 = document.getElementById("form-control-lg1");
+const formInput2 = document.getElementById("form-control-lg2");
+
+function formInputChange() {
+      if (window.visualViewport.width <= 600) {
+            formInput1.classList.remove("form-control-lg");
+            formInput2.classList.remove("form-control-lg");
+      } else {
+            formInput1.classList.add("form-control-lg");
+            formInput1.classList.add("form-control-lg");
+      }
+}
+
+formInputChange();
