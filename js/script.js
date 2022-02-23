7ae6077e1ac6514ff4737258d1ef71031c5686a4
César Augusto <ces.augusto108@gmail.com>
2022-02-23 11:27:31 -0300

    Ajustes no JS para o modal

--- js/script.js
+++ js/script.js
@@ -37,20 +37,43 @@ const login = document.getElementById("login");
 const modal = document.getElementById("modal");
 const modalCloseButton = document.getElementById("login-modal-close");
 
+const cssStyle = document.createElement("style");
+
+const styleText = document.createTextNode(`
+body::before {
+      content: '';
+      background-color: rgba(0, 0, 0, 0.5);
+      width: 100vw;
+      height: 100vh;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 3; 
+}`);
+
+cssStyle.appendChild(styleText);
+
 function modalShow() {
       modal.style.display = "block";
       nav.style.display = "none";
+
+      document.head.appendChild(cssStyle);
 }
 
 function modalClose() {
       modal.style.display = "none";
       nav.style.display = "block";
+
+      document.head.removeChild(cssStyle);
 }
 
 login.addEventListener("click", modalShow);
 modalCloseButton.addEventListener("click", modalClose);
 window.addEventListener("keydown", function (e) {
       if (e.key === "Escape") modalClose();
+});
+window.addEventListener("click", function (e) {
+      if (e.target === this.document.body) modalClose();
 });
 
 // efeito no footer
@@ -63,7 +86,6 @@ function footerGradient() {
       } else {
             footer.style.backgroundImage = "linear-gradient(to top, #000, #000)";
       }
-console.log(scrollY);
 }
 
-window.addEventListener('scroll', footerGradient);
+window.addEventListener("scroll", footerGradient);
